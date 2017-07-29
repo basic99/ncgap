b8e2089130e0dea7665e3f55545b1cecfcc2528a
Jim White <jbwhite2@ncsu.edu>
2017-07-29 17:55:37 -0400

    test script

---
+++ mapscript_info.php
@@ -0,0 +1,17 @@
+<HTML>
+<BODY>
+
+<?php
+  if (PHP_OS == "WINNT" || PHP_OS == "WIN32")
+  {
+    dl("php_mapscript.dll");
+  }
+  else
+  {
+    dl("php_mapscript.so");
+  }
+  phpinfo();
+?>
+
+</BODY>
+</HTML>
