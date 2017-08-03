f51c806f1c1c5c107c02c92fd32b5a9952f9983c
Jim White <jbwhite2@ncsu.edu>
2017-08-03 15:17:13 -0400

    test from book

---
+++ test/phpms_hello.php
@@ -0,0 +1,25 @@
+<?php
+
+  // Create a unique image name every time through
+
+  $image_name = sprintf("phpms-hello%0.6d",rand(0,999999)).".png";
+
+  // Create a new instance of a map object
+
+  $map = ms_newMapObj("/home/www/html/ncgap/test/hello.map");
+
+  // Create an image of the map and save it to disk
+
+  $image=$map->draw();
+  $image->saveImage("/var/www/html/ncgap/test/".$image_name);
+?>
+
+<html>
+<head><title>PHP MapScript Hello World</title></head>
+<body>
+  <form action="phpms_hello.php" method="POST">
+    <input type="image" name="img"
+          src="/ncgap/test/<?php echo $image_name; ?>">
+  </form>
+</body>
+</html>
