d9e5817f1c9a2e27afec8caee355383e74ba7944
Jim White <jbwhite2@ncsu.edu>
2017-10-26 16:27:39 -0400

    config

--- includes/nc_config.php
+++ includes/nc_config.php
@@ -7,11 +7,11 @@ $grass_raster = "/pub/grass/n_carolina/webserv/cellhd/";
 //location of GRASS raster data for permanent
 $grass_raster_perm = "/pub/grass/n_carolina/PERMANENT/cellhd/";
 
-$GISBASE = "/usr/local/grass-6.4.0svn";
+$GISBASE = "/usr/local/grass-6.4.6";
 
 $GISRC = "/var/www/html/ncgap/grassrc";
 
-$PATH = "/usr/local/grass-6.4.0svn/bin:/usr/local/grass-6.4.0svn/scripts:/usr/local/bin:/usr/bin:/bin";
+$PATH = "/usr/local/grass-6.4.6/bin:/usr/local/grass-6.4.6/scripts:/usr/local/bin:/usr/bin:/bin";
 
 $mspath = "/pub/server_temp/";
 
